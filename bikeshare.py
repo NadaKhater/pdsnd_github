fe07a2e23e798a5d4fa71259f39e85730aaa38df
NadaKhater <nadaalzahrani053@gmail.com>
2019-11-28 16:34:34 +0300

    docs : a new comment added in pikshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-
+# add a new comment in master branch
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
